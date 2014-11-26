1e230e874bccd67e2be90f7ced29c37b1d16b32b
rahulmadduluri <maddulur@usc.edu>
2014-11-25 16:37:46 -0800

    created card layout that can switch between LoginCard and GameCard

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,6 +20,12 @@ public class MainFrame extends JFrame {
 	static ChatPanel chatPanel;
 	
 	static JPanel outerPanel;
+	static JPanel loginCard;
+	static JPanel gameCard;
+	
+	//login/game card names
+	final static String LOGINCARD = "LOGIN";
+	final static String GAMECARD = "GAME";
 	
 	public MainFrame() {
 		super("Baseball Game");
@@ -26,15 +33,28 @@ public class MainFrame extends JFrame {
 		setSize(800,600);
 		
 		//panel initialization
-		outerPanel = new JPanel(new BorderLayout());
+		outerPanel = new  JPanel(new CardLayout());
+		gameCard = new JPanel(new BorderLayout());
+		loginCard = new LoginPanel();
 		gamePanel = new GamePanel();
 		chatPanel = new ChatPanel();
 		//lineupPanel = new LineupPanel();
 		
 		
-		outerPanel.add(gamePanel, BorderLayout.CENTER);
-		outerPanel.add(chatPanel, BorderLayout.SOUTH);
-		//outerPanel.add(lineupPanel, BorderLayout.SOUTH);
+		//add panels to game card
+		gameCard.add(gamePanel, BorderLayout.CENTER);
+		gameCard.add(chatPanel, BorderLayout.SOUTH);
+		//gameCard.add(lineupPanel, BorderLayout.SOUTH);
+		
+		//add cards to outer panel
+		outerPanel.add(loginCard, LOGINCARD);
+		outerPanel.add(gameCard, GAMECARD);
+		
+		//for right now just show game
+		CardLayout cl = (CardLayout)outerPanel.getLayout();
+		cl.show(outerPanel, GAMECARD);
+		
+		//add outer panel and make visible
 		add(outerPanel);
 		setVisible(true);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
