708f37416b242a52869dac76f967296d9c7e7493
Akshay Aggarwal <aggarwaa@usc.edu>
2014-11-29 15:29:55 -0800

    Created the batting and pitching animations. Added gif files and sprite classes.

--- src/Sprite.java
+++ src/Sprite.java
@@ -7,29 +7,78 @@ import javax.imageio.ImageIO;
 
 public class Sprite {
 
-    private static BufferedImage spriteSheet;
-     
+    private static BufferedImage spriteSheets;
+    static boolean isBatter;
+    boolean alreadyDone = false; 
+    
+    public Sprite (boolean isBatter){
+    	this.isBatter = isBatter;
+    }
 
     public static BufferedImage loadSprite() {
 
-        BufferedImage sprite = null;
-
+        BufferedImage sprites = null;
+    	/*BufferedImage spriteBat = null;
+        BufferedImage spritePitch = null;
+*/
         try {
         	String current_directory = System.getProperty("user.dir");
-            sprite = ImageIO.read(new File( "src/Images/BaseballSheet1.gif"));
+        	if (isBatter){
+        		sprites = ImageIO.read(new File("AAFP/src/Images/RBIBaseball94Sheet4.gif"));
+        	} else {
+        		sprites = ImageIO.read(new File("AAFP/src/Images/RBIBaseball94Sheet1.gif"));
+        	}
+            /*sprites[0] = spriteBat;
+            sprites[1] = spritePitch;*/
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return sprite;
+        return sprites;
     }
 
     public BufferedImage getSprite(int xGrid, int yGrid) {
-        if (spriteSheet == null) {
-            spriteSheet = loadSprite();
+        if (spriteSheets == null) {
+            spriteSheets = loadSprite(); //Just changed it to an array of bufferedImages
         }
 
-        return spriteSheet.getSubimage(xGrid *24 + 134, yGrid*112 + 50, 25, 40);
+        if (isBatter){
+	        if (xGrid >= 4 && !alreadyDone){
+	        	xGrid = xGrid - 4;
+	        	alreadyDone = true;
+	        }
+	        
+	        if (xGrid >= 0 && xGrid < 4 && yGrid <= 1){
+	        	if (yGrid == 1){
+	        		return spriteSheets.getSubimage(414+xGrid*38, 153 + yGrid*90, 30, 90);
+	        	} else
+	        	return spriteSheets.getSubimage(414+xGrid*38, 165 + yGrid*90, 30, 90);
+	        }
+	        else {
+	        	alreadyDone = false;
+	        	return spriteSheets.getSubimage(435+xGrid*38, 153 + yGrid*90, 30, 90);
+	        }
+        } else{
+        	 if (xGrid >= 13 && !alreadyDone){
+ 	        	xGrid = xGrid - 13;
+ 	        	alreadyDone = true;
+ 	        }
+ 	        
+        
+ 	        if (xGrid >= 0 && xGrid < 13 && yGrid <= 1){
+ 	        	if (yGrid == 1){
+ 	        		if (xGrid >= 8){
+ 	        			return spriteSheets.getSubimage(382+xGrid*29, 155 + yGrid*50, 32, 60);
+ 	        		} else
+ 	        			return spriteSheets.getSubimage(382+xGrid*32, 155 + yGrid*50, 28, 50);
+ 	        	} else
+ 	        		return spriteSheets.getSubimage(382+xGrid*27, 155 + yGrid*60, 27, 50);
+ 	        }
+ 	        else {
+ 	        	alreadyDone = false;
+	        		return spriteSheets.getSubimage(382+xGrid*27, 155 + yGrid*60, 27, 50);
+ 	        }
+        }
     }
 
 }
--- src/SpriteAnimation.java
+++ src/SpriteAnimation.java
@@ -1,4 +1,8 @@
 import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
@@ -8,42 +12,83 @@ import javax.swing.JLabel;
 
 
 public class SpriteAnimation extends Thread{
-	private Sprite[] sprites = new Sprite[5];
-	private int currentSprite = 0;
+	private Sprite[] sprites;
+	private int xSprite = 0, ySprite = 0;
 	public JLabel label;
+	boolean isBatter;
 	
-	public SpriteAnimation(JLabel jl) {
-		for (int i = 0; i < 5; i++) {
-			sprites[i] = new Sprite();
+	public SpriteAnimation(JLabel jl, boolean isBatter) {
+		this.isBatter = isBatter;
+		if (isBatter){ //Loading batter animation
+			sprites = new Sprite[14];
+			for (int i = 0; i < 14; i++) {
+				sprites[i] = new Sprite(isBatter);
+			}
+		} else{ //Loading pitcher animation
+			sprites = new Sprite[23];
+			for (int i = 0; i < 23; i++) {
+				sprites[i] = new Sprite(isBatter);
+			}
 		}
 		label = jl;
 	}
 
 
 	public void render() {
-		BufferedImage pic = sprites[currentSprite].getSprite(currentSprite+5,4);
-		BufferedImage resizedImage = new BufferedImage(200, 200, BufferedImage.TYPE_3BYTE_BGR);
+		BufferedImage pic = sprites[xSprite].getSprite(xSprite,ySprite);
+		BufferedImage resizedImage = new BufferedImage(200, 200, BufferedImage.TYPE_INT_ARGB);
+		
+		//Make the sprite transparent
+		GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice device = environment.getDefaultScreenDevice();
+		GraphicsConfiguration config = device.getDefaultConfiguration();
+		resizedImage = config.createCompatibleImage(200, 200, Transparency.BITMASK);
+		
+		
+		
 		label.setIcon(new ImageIcon(resizedImage));
 		label.revalidate();
 		label.repaint();
 		Graphics2D g = resizedImage.createGraphics();
 		g.drawImage(pic, 0, 0, 200, 200, null);
 		g.dispose();
-	
 	}
 
 	public void update() {
-		
-		currentSprite++;
-		if (currentSprite == 4)
-			currentSprite = 0;
-		
+		if (isBatter){
+			xSprite++;
+			if (xSprite == 4 && ySprite == 0){
+				//xSprite = 0;
+				ySprite++;
+			}
+			if (xSprite == 14 && ySprite == 1){
+				xSprite = 0;
+				ySprite = 0;
+			}
+		} else{
+			xSprite++;
+			if (xSprite == 13 && ySprite == 0){
+				//xSprite = 0;
+				ySprite++;
+			}
+			if (xSprite == 22 && ySprite == 1){
+				try {
+					sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				xSprite = 0;
+				ySprite = 0;
+				
+			}
+		}
 	}
 	public void run() {
-		while (true) {
+		for (int i = 0; i < sprites.length; i++){
 			render();
 			try {
-				sleep(500);
+				sleep(70);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
