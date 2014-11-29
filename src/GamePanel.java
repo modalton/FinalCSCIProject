53385d06bfaf74b90cc60abd272102290e2826d6
Akshay Aggarwal <aggarwaa@usc.edu>
2014-11-28 19:59:11 -0800

    Worked on GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,17 +2,25 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.Vector;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.JLabel;
 
 
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements ClientProcessInterface<GameMessage>{
 	
 	private static final long serialVersionUID = 1L;
 	Vector<Sprite> all_sprites = new Vector<Sprite>();
-	
+	boolean hasMessage;
+	boolean isBatting;
+	int x, y;
+	int scoreA, scoreB;
+	int base, inning; 
+	int strikes, outs;
+	boolean inningChange, pitChange, batChange;
+	boolean[] onBase; // what base people are on
+	boolean gameOver, aWins;
 
 	
 	public GamePanel(){
@@ -22,24 +30,46 @@ public class GamePanel extends JPanel{
 		add(label);
 		SpriteAnimation sa = new SpriteAnimation(label);
 		sa.start();
-		//legacy
-		/*
-		Sprite spritefactory = new Sprite();
-		BufferedImage pic = spritefactory.getSprite(2,4);
-		BufferedImage resizedImage = new BufferedImage(200, 200, BufferedImage.TYPE_3BYTE_BGR);
-		Graphics2D g = resizedImage.createGraphics();
-		g.drawImage(pic, 0, 0, 200, 200, null);
-		for (int i = 0; i < 10; i++) {
-			
-		}
-		g.dispose();
-		JLabel picLabel = new JLabel(new ImageIcon(resizedImage));
-		add(picLabel);
-		*/
 		
+		//Add Batting Box
+		//Enable user input
+		//Add action listener
+		
+		//HAVE TO KNOW WHAT TEAM THIS GAMEPANEL'S CLIENT IS ON
 
 		
 	}
+
+	//Add actionListener for pitcher/batter click in Batting Box
+	//Actoinlistner must toggle hasMessage
+	
+	@Override
+	public void processInputObject(GameMessage object) {
+		//Reads GameMessage
+		//Update scorebox and lineup panel depending on GameMessage from Sever
+		
+		isBatting = object.aBat;
+	}
+
+
+
+	@Override
+	public boolean hasOutputObject() {
+		// TODO Auto-generated method stub
+		return hasMessage;
+	}
+
+
+
+	@Override
+	public GameMessage processOutputObject() {
+		//Generate the message depending on where the player has clicked
+		return null;
+	}
+	
+	
+	
+	
 	
 	
 }
