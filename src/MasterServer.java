b6a216ff867e4bdac04ecd03dc5527f6a7db0bc4
Akshay Aggarwal <aggarwaa@usc.edu>
2014-12-01 00:31:17 -0800

    Chat bug resolved

--- src/MasterServer.java
+++ src/MasterServer.java
@@ -5,7 +5,7 @@ public class MasterServer {
 	public static void main(String[] args) throws IOException {
 		
     	Server<LoginMessage> login = new LoginServer(4444, 4);
-    	Server<Message> chat = new ChatServer(4445, 2);
+    	Server<Message> chat = new ChatServer(4445, 4);
     	Server<GameMessage> game = new GameServer(4446, 4);
     	login.start();
     	chat.start();
